ff49290385ca671f0147cad004cbc3c7ed8b894b
dagyeong <ekrud926@naver.com>
2023-02-21 14:52:17 +0900

    users

---
+++ ppoy/src/ppoy/Users.java
@@ -0,0 +1,48 @@
+package ppoy;
+
+public class Users {
+
+	private String userId;
+	private String userName;
+	private String userPw;
+	private String userTel;
+	
+	//Getter
+	public String getUserId() {
+		return userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public String getUserTel() {
+		return userTel;
+	}
+	
+	//Setter
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", userName=" + userName + ", userPw=" + userPw + ", userTel=" + userTel
+				+ "]";
+	}
+	
+
+}
